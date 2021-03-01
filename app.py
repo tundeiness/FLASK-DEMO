eb9742db94c2eef2633b9ef1dc1116131b819429
tundeiness <tundeiness@yahoo.co.uk>
2021-03-01 13:01:25 +0100

    setup POST route
    pass data into POST route
    
    access data from post.html
    
    loop over data for rendering

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ from flask import Flask, render_template
 # create flask app
 app = Flask(__name__)
 
+
+all_posts =[
+    {
+    'title': 'Post 1',
+    'content': 'This is the content of post 1'
+    },
+    {
+    'title': 'Post 2',
+    'content': 'This is the content of post too'
+    }
+]
+
 # define route
 @app.route('/')
 def index():
     return render_template('main.html')
     # return Path('index.html').read_bytes();
+
+@app.route('/posts')
+def posts():
+    return render_template('post.html', posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:idx>')
 #code that will be run when the route is called
