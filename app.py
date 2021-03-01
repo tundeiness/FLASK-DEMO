23daa9bb38cdc008d2765d616284a87a3eeca9a1
tundeiness <tundeiness@yahoo.co.uk>
2021-03-01 11:43:57 +0100

    create route for only GET request

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 
+# create flask app
 app = Flask(__name__)
 
-@app.route('/')
-def greetings():
-    return "hello World"
+# define route
+# @app.route('/')
+@app.route('/home/users/<string:name>/posts/<int:idx>')
+#code that will be run when the route is called
+def greetings(name, idx):
+    return "hello, " + name + ". This is your " + str(idx) + " post"
 
+@app.route('/onlyget', methods=['GET'])
+def getrequest():
+    return 'you can get this using get request'
 
+# python best practices
 if __name__ == '__main__':
     app.run(debug=True) 
