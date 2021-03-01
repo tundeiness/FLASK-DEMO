3f8f2a3c50250b05a18041ae08c7d5907e9a531d
tundeiness <tundeiness@yahoo.co.uk>
2021-03-01 13:14:26 +0100

    use if clause to get content within for loop

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 all_posts =[
     {
     'title': 'Post 1',
-    'content': 'This is the content of post 1'
+    'content': 'This is the content of post 1',
+    'author':'Tunde'
     },
     {
     'title': 'Post 2',
